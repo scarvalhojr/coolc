0e8d00b7675a2729a5e888247f4a89107efa4379
Sergio de Carvalho <scarvalhojr@gmail.com>
2022-03-04 21:24:03 +0000

    Fix printing of double quotes in string literal tokens

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,7 @@
 pub fn escape_str(s: &str) -> String {
     s.chars().fold(String::new(), |mut string, ch| {
         match ch {
+            '"' => string.push_str("\\\""),
             '\\' => string.push_str(r"\\"),
             '\n' => string.push_str(r"\n"),
             '\t' => string.push_str(r"\t"),
